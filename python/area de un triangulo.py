702b3f5d0f5e8c39c6f72a23eea4d94bc4070bfa
OlivxresDylxn <dylanov03@gmail.com>
2026-09-18 19:28:08 -0600

    Delete python directory

--- python/area de un triangulo.py
+++
@@ -1,9 +0,0 @@
-import os
-print("bienvenido al calculador de altura y base de un rectangulo")
-alt= int(input("indique su altura\n"))
-
-base= int(input("indique su base\n"))
-
-operacion= alt * base
-os.system("pause")
-print(f"el area de su triangullo es de {operacion}")
--- python/cuadrado de un numero.py
+++
@@ -1,9 +0,0 @@
-import os
-print("bienvenido como sacar el cuadrado a un numero")
-
-num= int(input("indique el numero al que le desea sacar el cuadrado\n"))
-
-operacion= num * num
-os.system("pause")
-os.system("cls")
-print(f"el cuadrado del numero {num} es {operacion}")
